1daf036b9129fb7e7d736ec2fee38efdc8535406
Christian Braybrooke <chris@purplemountmedia.com>
2018-09-25 11:49:03 +0100

    Add intercom

--- storage/framework/views/18ddafe7af050261ef332f3aa065cea8bf2e1c45.php
+++ storage/framework/views/18ddafe7af050261ef332f3aa065cea8bf2e1c45.php
@@ -44,5 +44,13 @@
     <script src="<?php echo e(mix('js/vendor.js')); ?>"></script>
     <script src="<?php echo e(mix('js/web.js')); ?>"></script>
     <?php echo $__env->yieldPushContent('scripts'); ?>
+
+    <script>
+      window.intercomSettings = {
+        app_id: "n2sb33ro",
+        background_color: '#008080'
+      };
+    </script>
+    <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/n2sb33ro';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
 </body>
 </html>
