966443f744367e9c06bb1126e41dfa155eaff165
Jake Causier <jake@purplemountmedia.com>
2018-09-26 10:37:36 +0100

    Minor style change - B2 header colour

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,8 @@
 @extends('layouts.app')
 
 @section('header')
-  <div id="site_product_header" class="bg-app">
-    <div class="container py-3">
-      <div class="row">
-        <div class="col-lg-8 col-12 mx-auto">
-          <div class="product_header justify-between">
-            <img src="{{ getB2Config('logo') }}" class="site_logo" alt="B2 Systems">
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  @component('_partials.dash-header')
+  @endcomponent
 @endsection
 
 @section('content')
--- resources/views/schools.blade.php
+++ resources/views/schools.blade.php
@@ -1,14 +1,8 @@
 @extends('layouts.app')
 
 @section('header')
-  <div id="site_product_header" class="bg-app">
-    <div class="container py-3">
-        <div class="product_header justify-between">
-          <img src="{{ getB2Config('logo') }}" class="site_logo" alt="B2 Systems">
-          <h3 class="ml-2 mb-0 text-white site_product">Education Inventory Platform</h3>
-        </div>
-    </div>
-  </div>
+  @component('_partials.dash-header')
+  @endcomponent
 @endsection
 
 @section('content')
